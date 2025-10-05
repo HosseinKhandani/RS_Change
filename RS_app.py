52f93895b8645f3d1adc1f712bcb88e5a3061f6e
DataScience <43822230+HosseinKhandani@users.noreply.github.com>
2025-10-05 12:54:08 +0330

    Update RS_app.py

--- RS_app.py
+++ RS_app.py
@@ -1598,11 +1598,11 @@ with tab1:
 
 with tab2:
     #st.header("Before Image Analysis")
-    with tab2:
+    
     st.markdown("<h1 style='color: #667eea;'>📊 تحلیل تصویر قبل از تغییر</h1>", unsafe_allow_html=True)
     
     # کارت وضعیت
-    if 'reconstructed_before_image' in st.session_state:
+if 'reconstructed_before_image' in st.session_state:
         st.markdown("""
         <div class='stat-card success'>
             <h3>✅ وضعیت پردازش</h3>
@@ -2501,3 +2501,4 @@ with tab4: # This line is commented out as the code below is the content of tab4
             st.error(traceback.format_exc())
     else:
         st.info("After applying erosion, the interactive map will appear here.")
+
